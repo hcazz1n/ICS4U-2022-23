7bfc6389e6fe7b20d86b51e5a031d8dd434caa23
Harrison Cazzin <hcazzin@bayviewglen.ca>
2022-11-01 10:28:40 -0400

    javascript intro + new project

---
+++ Intro to JavaScript/jaav.js
@@ -0,0 +1,12 @@
+team = {
+    id : 1,
+    city : 'Toronto',
+    nickname : 'BBBBBBBBB',
+    wins : 6,
+    losses : 3,
+    games : [
+        {
+            opponent : 7 //id
+        }
+    ]
+}
