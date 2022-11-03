3c11bc0ef28d8342ee91c892b78e8a60702a891c
Harrison Cazzin <hcazzin@bayviewglen.ca>
2022-11-03 17:49:37 -0400

    about to work on script.js

--- ProjectS/script.js
+++ ProjectS/script.js
@@ -4,298 +4,272 @@ function getTeams() {
    teamData = JSON.parse(localStorage['teams']);
 }
 
-
-let teams = [];
+let teamsGroupA = [];
+let teamsGroupB = [];
+let teamsGroupC = [];
+let teamsGroupD = [];
 
 let team = {};
 team['name'] = '100 Thieves';
-team['id'] = 1;
+team['id'] = 'D4';
 team['tag'] = '100';
 team['W'] = 1;
 team['L'] = 5;
 team['winrate'] = 17;
+team['duration'] = 0;
 team['killsPerGame'] = 8.8;
-team['duration'] = 1819;
 team['goldDiffPerMin'] = -264;
 team['games'] = [{'opp': 'CFO'}, {'opp': 'GEN'}, {'opp': 'RNG'}, {'opp': 'CFO'}, {'opp': 'RNG'}, {'opp': 'GEN'}, ];
-teams.push(team);
+teamsGroupD.push(team);
 
 let team = {};
 team['name'] = 'Cloud9';
-team['id'] = 2;
+team['id'] = 'A4';
 team['tag'] = 'C9';
 team['W'] = 1;
 team['L'] = 5;
 team['winrate'] = 17;
+team['duration'] = 0;
 team['killsPerGame'] = 6.7;
-team['duration'] = 1632;
 team['goldDiffPerMin'] = -360;
 team['games'] = [{'opp': 'FNC'}, {'opp': 'EDG'}, {'opp': 'T1'}, {'opp': 'FNC'}, {'opp': 'EDG'}, {'opp': 'T1'}, ];
-teams.push(team);    
+teamsGroupA.push(team);    
 
 let team = {};
 team['name'] = 'CTBC Flying Oyster';
-team['id'] = 3;
+team['id'] = 'D3';
 team['tag'] = 'CFO';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupD.push(team);  
 
 let team = {};
 team['name'] = 'DRX';
-team['id'] = 4;
+team['id'] = 'C1';
 team['tag'] = 'DRX';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupC.push(team);  
 
 let team = {};
 team['name'] = 'DWG KIA';
-team['id'] = 5;
+team['id'] = 'B2';
 team['tag'] = 'DK';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupB.push(team);  
 
 let team = {};
 team['name'] = 'Edward Gaming';
-team['id'] = 6;
+team['id'] = 'A2';
 team['tag'] = 'EDG';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupA.push(team);  
 
 let team = {};
 team['name'] = 'Evil Geniuses';
-team['id'] = 7;
+team['id'] = 'B3';
 team['tag'] = 'EG';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupB.push(team);  
 
 let team = {};
 team['name'] = 'Fnatic';
-team['id'] = 8;
+team['id'] = 'A3';
 team['tag'] = 'FNC';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team);  
+teamsGroupA.push(team);  
 
 let team = {};
 team['name'] = 'G2 Esports';
-team['id'] = 9;
+team['id'] = 'B4';
 team['tag'] = 'G2';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupB.push(team); 
 
 let team = {};
 team['name'] = 'GAM Esports';
-team['id'] = 10;
+team['id'] = 'C4';
 team['tag'] = 'GAM';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupC.push(team); 
 
 let team = {};
 team['name'] = 'Gen. G eSports';
-team['id'] = 11;
+team['id'] = 'D1';
 team['tag'] = 'GEN';
 team['W'] = 10;
 team['L'] = 6;
 team['winrate'] = 63;
+team['duration'] = 0;
 team['killsPerGame'] = 0;
-
-
-team['duration'] = 1884;
 team['goldDiffPerMin'] = 111;
 team['games'] = [{
         'opp': 14
     }, {
         'opp': 1
     }];
-teams.push(team); 
+teamsGroupD.push(team); 
 
 let team = {};
 team['name'] = 'JD Gaming';
-team['id'] = 12;
+team['id'] = 'B1';
 team['tag'] = 'JDG';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupB.push(team); 
 
 let team = {};
 team['name'] = 'Rogue';
-team['id'] = 13;
+team['id'] = 'C1';
 team['tag'] = 'RGE';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupC.push(team); 
 
 let team = {};
 team['name'] = 'Royal Never Give Up';
-team['id'] = 14;
+team['id'] = 'D2';
 team['tag'] = 'RNG';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupD.push(team); 
 
 let team = {};
 team['name'] = 'T1';
-team['id'] = 15;
+team['id'] = 'A1';
 team['tag'] = 'T1';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupA.push(team); 
 
 let team = {};
 team['name'] = 'Top Esports';
-team['id'] = 16;
+team['id'] = 'C3';
 team['tag'] = 'TES';
 team['W'] = 0;
 team['L'] = 0;
 team['winrate'] = 0;
-team['killsPerGame'] = 0;
-
-
 team['duration'] = 0;
+team['killsPerGame'] = 0;
 team['goldDiffPerMin'] = 0;
 team['games'] = [{
         'opp': 1
     }, {
         'opp': 2
     }];
-teams.push(team); 
+teamsGroupC.push(team); 
 
