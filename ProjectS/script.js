cd5d49037c98ae6057d79a8d13d40a1753cd9a3b
Harrison Cazzin <hcazzin@bayviewglen.ca>
2022-11-09 09:04:48 -0500

    aa

--- ProjectS/script.js
+++ ProjectS/script.js
@@ -5,7 +5,6 @@ let groupB;
 let groupC;
 let groupD;
 
-
 function getTeams() {
     groupA = JSON.parse(localStorage['teamsGroupA']);
     groupB = JSON.parse(localStorage['teamsGroupB']);
@@ -22,7 +21,8 @@ function createTables(){
     createTable(groupD, 'D');
 }
 
-ascendingRank = true;
+let ascendingRankA, ascendingRankB, ascendingRankC, ascendingRankD = true;
+let ascendingNameA, ascendingNameB, ascendingNameC, ascendingNameD = false;
 
 function createTable(teams, tableOfChoice) {
     tableBody = document.querySelector(selectedTable(tableOfChoice));
@@ -51,8 +51,8 @@ function createTable(teams, tableOfChoice) {
         td.textContent = ((team.W / (team.W + team.L)) * 100).toFixed(2) + "%";
         row.appendChild(td);
 
-        td = document.createElement('GDM');
-        td.textContent = team.GDM;
+        td = document.createElement('KPG');
+        td.textContent = team.killsPerGame;
         row.appendChild(td);
 
         tableBody.appendChild(row);
@@ -71,15 +71,88 @@ function selectedTable(table) {
     }
 }
 
-function sort(data){
-    if(data === 'rank'){
-        if(ascendingRank){
-            console.log('apple');
-            ascendingRank = false;
-            data = teamsGroupA.sort((teamA, teamB) => (teamB.rank - teamA.rank));
-        } else {
-            ascendingRank = true;
-            data = teamsGroupA.sort((teamA, teamB) => teamA.rank - teamB.rank);
+function sort(data, table){
+    if(table == 'A'){
+        if(data === 'rank'){
+            if(ascendingRankA){
+                ascendingRankA = false;
+                groupA = groupA.sort((teamA, teamB) => (teamB.rank - teamA.rank));
+            } else {
+                ascendingRankA = true;
+                groupA = groupA.sort((teamA, teamB) => teamA.rank - teamB.rank);
+            }
+        } else if(data === 'name'){
+            if(ascendingNameA){
+                ascendingNameA = false;
+                groupA = groupA.sort((teamA, teamB) => {return (teamA.name > teamB.name) ? -1 : 1});
+            } else {
+                ascendingNameA = true;
+                groupA = groupA.sort((teamA, teamB) => {return (teamA.name < teamB.name) ? -1 : 1});
+            }
+        }
+    } else if(table == 'B'){
+        if(data === 'rank'){
+            if(ascendingRankB){
+                ascendingRankB = false;
+                groupB = groupB.sort((teamA, teamB) => (teamB.rank - teamA.rank));
+            } else {
+                ascendingRankB = true;
+                groupB = groupB.sort((teamA, teamB) => teamA.rank - teamB.rank);
+            }
+        } else if(data === 'name'){
+            if(ascendingNameB){
+                ascendingNameB = false;
+                groupB = groupB.sort((teamA, teamB) => {return (teamA.name > teamB.name) ? -1 : 1});
+            } else {
+                ascendingNameB = true;
+                groupB = groupB.sort((teamA, teamB) => {return (teamA.name < teamB.name) ? -1 : 1});
+            }
+        }
+    } else if(table == 'C'){
+        if(data === 'rank'){
+            if(ascendingRankC){
+                ascendingRankC = false;
+                groupC = groupC.sort((teamA, teamB) => (teamB.rank - teamA.rank));
+            } else {
+                ascendingRankC = true;
+                groupC = groupC.sort((teamA, teamB) => teamA.rank - teamB.rank);
+            }
+        } else if(data === 'name'){
+            if(ascendingNameC){
+                ascendingNameC = false;
+                groupC = groupC.sort((teamA, teamB) => {return (teamA.name > teamB.name) ? -1 : 1});
+            } else {
+                ascendingNameC = true;
+                groupC = groupC.sort((teamA, teamB) => {return (teamA.name < teamB.name) ? -1 : 1});
+            }
+        }
+    } else {
+        if(data === 'rank'){
+            if(ascendingRankD){
+                ascendingRankD = false;
+                groupD = groupD.sort((teamA, teamB) => (teamB.rank - teamA.rank));
+            } else {
+                ascendingRankD = true;
+                groupD = groupD.sort((teamA, teamB) => teamA.rank - teamB.rank);
+            }
+        } else if(data === 'name'){
+            if(ascendingNameD){
+                ascendingNameD = false;
+                groupD = groupD.sort((teamA, teamB) => {return (teamA.name > teamB.name) ? -1 : 1});
+            } else {
+                ascendingNameD = true;
+                groupD = groupD.sort((teamA, teamB) => {return (teamA.name < teamB.name) ? -1 : 1});
+            }
         }
     }
+
+    if(table == 'A')
+        createTable(groupA, 'A');
+    else if(table == 'B')
+        createTable(groupB, 'B');
+    else if(table == 'C')
+        createTable(groupC, 'C');
+    else
+        createTable(groupD, 'D');
+
 }
