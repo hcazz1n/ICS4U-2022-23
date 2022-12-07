f50c192b50bd3e349f7d7dfe94ae55bd052d1489
Harrison Cazzin <hcazzin@bayviewglen.ca>
2022-12-07 17:55:39 -0500

    PROMISES AND AUTHENTICATION

---
+++ Crash Course Code/JavaScript/promises.js
@@ -0,0 +1,16 @@
+function getPosts(){
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post, callback){
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
