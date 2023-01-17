576046345ce0dc41a9a81d4be76eed207c652647
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-01-17 09:37:06 -0500

    fixtraversal

--- DataStructures/src/IntBST.java
+++ DataStructures/src/IntBST.java
@@ -6,7 +6,72 @@ public class IntBST {
     }
 
     public IntBSTNode add(Integer val){
+        if(root == null){
+            root = new IntBSTNode(val);
+        }
         return addRecursive(root, val);
+    }
+
+    public IntBSTNode find(Integer val){
+        return findRecursive(root, val);
+    }
+
+    public void preOrderPrintTraversal(){
+        preOrderPrintTraversal(root);
+    }
+
+    private void preOrderPrintTraversal(IntBSTNode root){
+        System.out.println(root.getValue());
+        if(root.hasLeftChild()){
+            preOrderPrintTraversal(root.getLeftChild());
+        } 
+
+        if(root.hasRightChild()){
+            preOrderPrintTraversal(root.getRightChild());
+        }
+    }
+
+    public void postOrderPrintTraversal(){
+        postOrderPrintTraversal(root);
+    }
+
+    private void postOrderPrintTraversal(IntBSTNode root){
+        if(root.hasLeftChild()){
+            postOrderPrintTraversal(root.getLeftChild());
+        } 
+
+        if(root.hasRightChild()){
+            postOrderPrintTraversal(root.getRightChild());
+        }
+
+        System.out.println(root.getValue());
+    }
+
+    public void inOrderPrintTraversal(){
+        inOrderPrintTraversal(root);
+    }
+
+    private void inOrderPrintTraversal(IntBSTNode root){
+        if(root.hasLeftChild()){
+            inOrderPrintTraversal(root.getLeftChild());
+        } 
+
+        System.out.println(root.getValue());
+
+        if(root.hasRightChild()){
+            inOrderPrintTraversal(root.getRightChild());
+        }
+    }
+
+    private IntBSTNode findRecursive(IntBSTNode root, Integer val) {
+        if(val == root.getValue()) 
+            return root;
+        else if(root.hasLeftChild() && root.getValue() > val)
+            return findRecursive(root.getLeftChild(), val);
+        else if(root.hasRightChild() && root.getValue() < val)
+            return findRecursive(root.getRightChild(), val);
+        else 
+            return null;
     }
 
     /**
