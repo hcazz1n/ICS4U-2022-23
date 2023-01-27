5d8509a54f20c26451c4a4b921e91222b6da7d4b
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-01-27 00:16:27 -0500

    VUEJSCRASHCOURSE

---
+++ VueJS/task-tracker/src/components/MainHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonComp />
+    </header>
+</template>
+
+<script>
+    import ButtonComp from './ButtonComp'
+
+    export default {
+        name: 'MainHeader',
+        props: {
+            title: String,
+        },
+        components: {
+            ButtonComp
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
