e484118a98665a0b37a69ac7746d1033aff8d422
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-02-22 22:56:22 -0500

    new components - lesson 2

--- VueJS/vue-travel-app/src/App.vue
+++ VueJS/vue-travel-app/src/App.vue
@@ -2,33 +2,16 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <br>
-    <router-view to="/">Home</router-view> |
-    <router-view to="/about">About</router-view>
-    <router-view></router-view>
-    
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
