dfdf9e25360784b1350e75da188b147cad5d91f1
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-02-24 13:39:16 -0500

    vue router 4 lsn 2 done

--- VueJS/vue-travel-app/src/router/index.js
+++ VueJS/vue-travel-app/src/router/index.js
@@ -3,17 +3,26 @@ import Home from '../views/Home.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/brazil', name: 'Brazil', component: ()=>import('../views/Brazil.vue')},
-    {path: '/hawaii', name: 'Hawaii', component: ()=>import('../views/Hawaii.vue')},
-    {path: '/jamaica', name: 'Jamaica', component: ()=>import('../views/Jamaica.vue')},
-    {path: '/panama', name: 'Panama', component: ()=>import('../views/Panama.vue')},
-    {path: '/destination/:id', component: ()=>import('../views/DestinationShow.vue')},
+    {
+    path: '/destination/:id/:slug', 
+    name: 'destination.show', 
+    component: ()=>import('../views/DestinationShow.vue'),
+    props: route => ({id: parseInt(route.params.id)}),
+    children: [
+        {
+            path: ':experienceSlug',
+            name: 'experience.show',
+            component: () => import('../views/ExperienceShow.vue'),
+            props: route => ({...route.params, id: parseInt(route.params.id)})
+        } 
+    ]
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
-    linkActiveClass: 'active-page'
+    //linkActiveClass: 'active-page'
 })
 
 export default router
--- VueJS/vue-travel-app/src/views/DestinationShow.vue
+++ VueJS/vue-travel-app/src/views/DestinationShow.vue
@@ -1,23 +1,42 @@
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`">
             <p>{{ destination.description }}</p>
         </div>
     </section>
+    <section class="experiences">
+        <h2>Top Experiences in {{ destination.name }}</h2>
+        <div class="cards">
+            <router-link
+            v-for="experience in destination.experiences"
+            :key="experience.slug"
+            :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+            >
+                <ExperienceCard 
+                :experience="experience"
+                />
+            </router-link>
+        </div>
+        <router-view />
+    </section>
 </template>
 
 <script>
 import sourceData from '../data.json'
+import ExperienceCard from '../components/ExperienceCard.vue'
 export default {
+    components:{ExperienceCard}, 
+    props:{
+        id: {type: Number, required: true}
+    },
     computed:{
-        destinationId(){
-            return parseInt(this.$route.params.id)
-        },
         destination(){
-            return sourceData.destinations.find(destination => destination.id === this.destinationId)
-        }
-    }
+            return sourceData.destinations.find(
+                (destination) => destination.id === this.id
+            );
+        },
+    },
 }
 </script>
