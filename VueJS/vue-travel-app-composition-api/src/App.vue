a955b38accd4647af48333d7bf39b8c7ec313cbe
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-02-28 08:38:57 -0500

    new vue-travel-app with comp api

---
+++ VueJS/vue-travel-app-composition-api/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <TheNavigation/>
+      <br>
+      
+  <div class="container">
+    <router-view v-slot="{Component}">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+  </router-view>
+
+  </div>
+</template>
+
+<script>
+import TheNavigation from './components/TheNavigation.vue'
+export default{
+  components: {TheNavigation}
+}
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ VueJS/vue-travel-app-composition-api/src/components/AppLink.vue
@@ -0,0 +1,18 @@
+<template>
+    <a v-if="isExternal" class="external-link" target="_blank" rel="noopener" :href="to"><slot/></a>
+    <router-link v-else class="internal-link" v-bind="$props"><slot/></router-link>
+</template>
+
+<script>
+import {RouterLink} from 'vue-router'
+export default{
+    props:{
+        ...RouterLink.props
+    },
+    computed:{
+        isExternal(){
+            return typeof this.to === 'string' && this.to.startsWith('http')
+        }
+    }
+}
+</script>
